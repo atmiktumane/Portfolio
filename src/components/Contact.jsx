bde9a1b46f593a77d5210c42e9a9f1e30cc266c8
atmiktumane <atmiktumane72@gmail.com>
2024-06-22 14:22:54 +0530

    Contact Page Functionality - to send "Contact Form" data through "Web3Forms"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setResult("Sending....");
+
+    // store form data in "formData"
+    const formData = new FormData(e.target);
+
+    formData.append("access_key", "24f7b493-9f86-448e-ac5f-742962bebce2");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      e.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
-    <div className="h-[100%] px-4 md:px-9 pt-20 pb-[53px]">
+    <div className="h-[100%] px-4 md:px-9 pt-20 pb-[25px]">
       {/* Heading */}
       <h2 className="text-2xl font-medium text-purple-300">Contact Us</h2>
 
@@ -10,7 +36,10 @@ const Contact = () => {
       <div className="mt-3 w-8 h-[3px] bg-purple-500 rounded-2xl"></div>
 
       {/* Contact Form */}
-      <form className="mt-12  bg-zinc-900 rounded-lg p-5 py-9 flex flex-col gap-7">
+      <form
+        onSubmit={onSubmit}
+        className="mt-12  bg-zinc-900 rounded-lg p-5 py-9 flex flex-col gap-7"
+      >
         {/* Name & Email */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
           {/* Name */}
@@ -58,6 +87,9 @@ const Contact = () => {
         >
           Submit Form
         </button>
+
+        {/* Data Sending Loader */}
+        <span className="text-center text-zinc-700">{result}</span>
       </form>
 
       {/* Note */}
